af3b3bd94e0d95b773437638fc88fbe8e2224a07
Kolby Kruger <kolbykruger@gmail.com>
2020-07-09 17:48:43 -0400

    added generative shapes

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,17 @@
 <template>
-<main>
+<main id="home" @click="regenerateShapes">
+
+	<div class="shapes" ref="shapes"></div>
 
 	<section class="impression" ref="impression" @mouseenter="mouseEnter" @mouseleave="mouseLeave" @mousemove="mouseMove">
 		<div class="container">
 
-			<div class="impression__background">
-				<div class="impression__background--layer-1 impression__background--timing-1" v-html="Arc"></div>
-				<div class="impression__background--layer-2 impression__background--timing-1" v-html="Arc"></div>
-			</div>
-
 			<div class="impression__contents">
-				<p class="impression__intro">hello,</p>
-				<p class="impression__intro"><span>my&nbsp;</span><span>name&nbsp;</span><span>is&nbsp;</span></p>
-				<h1>
-					<span class="visually-hidden">Kolby Kruger</span>
-					<span>k</span>
-					<span>o</span>
-					<span>l</span>
-					<span>b</span>
-					<span>y</span>
-					<span>&nbsp;</span>
-					<span>k</span>
-					<span>r</span>
-					<span>u</span>
-					<span>g</span>
-					<span>e</span>
-					<span>r</span>
-				</h1>
-				<p class="impression__summary">I'm a {{ age }} year old designer & developer from Vermont. I aspire to create noteworthy, performant, and custom websites.</p>
-			</div>
-
-			<div class="buttons">
-				<nuxt-link to="/work"><span>Explore my work</span></nuxt-link>
-				<nuxt-link to="/about"><span>More about me</span></nuxt-link>
+				<h1>Hi, I'm Kolby.</h1>
+				<p class="impression__summary">I'm a {{ age }} year old designer & developer from Vermont. I create noteworthy, performant, and custom websites.</p>
+				<div class="buttons">
+					<nuxt-link to="/work">Explore my work <span class="arrow">-></span></nuxt-link>
+				</div>
 			</div>
 
 		</div>
@@ -89,6 +68,8 @@ export default {
 				$el.linkMove(link, cursor, event, $el)
 			})
 		})
+
+		this.regenerateShapes();
 	},
 	methods: {
 		mouseEnter() {
@@ -124,14 +105,66 @@ export default {
 			})
 		},
 		linkMove(link, cursor, event, $el) {
-			const rel = {
-				x: (event.clientX - $el.linkDimensions.left) - ($el.linkDimensions.width / 2),
-				y: (event.clientY - $el.linkDimensions.top) - ($el.linkDimensions.height / 2)
-			}
-			TweenMax.to(link, 1.2, {
-				x: rel.x / $el.linkDimensions.width * 40,
-				y: rel.y / $el.linkDimensions.height * 30,
+			requestAnimationFrame(() => {
+				const rel = {
+					x: (event.clientX - $el.linkDimensions.left) - ($el.linkDimensions.width / 2),
+					y: (event.clientY - $el.linkDimensions.top) - ($el.linkDimensions.height / 2)
+				}
+				TweenMax.to(link, 1.2, {
+					x: rel.x / $el.linkDimensions.width * 25,
+					y: rel.y / $el.linkDimensions.height * 25,
+				});
 			});
+		},
+		regenerateShapes() {
+
+			const shapeCount = 40;
+			const shapes = this.$refs.shapes;
+			const shapeList = shapes.querySelectorAll('.shape');
+			const shapeOptions = ['shape__circle', 'shape__triangle', 'shape__triangle-circle']
+			const type = ['shape__circle--half', 'shape__circle--quarter', 'shape__circle--full']
+			const rotation = ['90', '180', '270'];
+			const colors = ['primary', 'secondary', 'tertiary'];
+			const size = [1, 2];
+			const timing = [1, 2, 3];
+			const delay = [1, 2, 3];
+
+			shapes.classList.remove('animate-in')
+			shapes.innerHTML = '';
+			let lastSize = 1;
+
+			for (let i = 0; i < shapeCount; i++) {
+
+				let shape = document.createElement('div');
+				shape.classList.add('shape');
+				shape.classList.add(shapeOptions[Math.floor(Math.random() * shapeOptions.length)])
+				shape.setAttribute('data-rotation', rotation[Math.floor(Math.random() * rotation.length)])
+				shape.setAttribute('data-color', colors[Math.floor(Math.random() * colors.length)])
+				shape.setAttribute('data-timing', timing[Math.floor(Math.random() * timing.length)])
+				shape.setAttribute('data-delay', delay[Math.floor(Math.random() * delay.length)])
+
+				const randomSize = size[Math.floor(Math.random() * size.length)];
+				if (lastSize == 2) {
+					shape.setAttribute('data-size', 1)
+					lastSize = 1;
+				} else {
+					shape.setAttribute('data-size', randomSize)
+					lastSize = randomSize;
+				}
+
+				if (shape.classList.contains('shape__circle')) {
+					shape.classList.add(type[Math.floor(Math.random() * type.length)]);
+				}
+				if (shape.classList.contains('shape__triangle-circle')) {
+					shape.setAttribute('data-color-secondary', colors[Math.floor(Math.random() * colors.length)])
+				}
+
+				shapes.appendChild(shape)
+			}
+
+			setTimeout(function() {
+				shapes.classList.add('animate-in');
+			}, 50)
 		}
 	}
 }
