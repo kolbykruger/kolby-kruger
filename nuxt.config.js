a4551f41c6c9d9d5bc13c25214985f8b9800f6be
Kolby Kruger <kolbykruger@gmail.com>
2020-07-17 17:28:22 -0400

    testing netlify forms & sitemap generator

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,65 @@
 export default {
-    mode: 'universal',
+	mode: 'universal',
 
-    head: {
-        title: 'Kolby Kruger',
-        meta: [
-            { charset: 'utf-8' },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content:
-                    'Kolby Kruger is a freelance designer, developer and student that does web design and web development in Burlington, Vermont.',
-            },
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    },
+	head: {
+		title: 'Kolby Kruger',
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content:
+					'Kolby Kruger is a freelance designer, developer and student that does web design and web development in Burlington, Vermont.',
+			},
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-    loading: {
-        color: '#fff',
-    },
+	loading: {
+		color: '#fff',
+	},
 
-    css: ['@/static/fonts/moderat/moderat.css'],
+	sitemap: {
+		path: '/sitemap.xml',
+		hostname: 'https://kolby.dev',
+		gzip: true,
+		defaults: {
+			changefreq: 'daily',
+			priority: 1,
+			lastmod: new Date(),
+		},
+	},
 
-    plugins: [
-        '@/assets/css/bundle.scss',
-        'node_modules/flickity/dist/flickity.min.css',
-        '@/plugins/vue-lazysizes.client.js',
-        '@/plugins/LazySizes',
-    ],
+	css: ['@/static/fonts/moderat/moderat.css'],
 
-    modules: [
-        '@/modules/static',
-        '@/modules/crawler',
-        '@nuxtjs/svg',
-        '@nuxtjs/prismic',
-    ],
+	plugins: [
+		'@/assets/css/bundle.scss',
+		'node_modules/flickity/dist/flickity.min.css',
+		'@/plugins/vue-lazysizes.client.js',
+		'@/plugins/LazySizes',
+	],
 
-    prismic: {
-        endpoint: 'https://kolby-kruger.cdn.prismic.io/api/v2',
-        linkResolver: '@/plugins/link-resolver',
-        htmlSerializer: '@/plugins/html-serializer',
-    },
+	modules: ['@/modules/static', '@/modules/crawler', '@nuxtjs/svg', '@nuxtjs/prismic', '@nuxtjs/sitemap'],
 
-    build: {
-        extend(config, { isDev, isClient, loaders: { vue } }) {
-            config.resolve.alias['vue'] = 'vue/dist/vue.common';
-            vue.transformAssetUrls.img = ['data-src', 'src'];
-            vue.transformAssetUrls.source = ['data-srcset', 'srcset'];
-        },
-    },
+	prismic: {
+		endpoint: 'https://kolby-kruger.cdn.prismic.io/api/v2',
+		linkResolver: '@/plugins/link-resolver',
+		htmlSerializer: '@/plugins/html-serializer',
+	},
 
-    generate: {
-        fallback: '404.html',
-    },
+	build: {
+		extend(config, { isDev, isClient, loaders: { vue } }) {
+			config.resolve.alias['vue'] = 'vue/dist/vue.common';
+			vue.transformAssetUrls.img = ['data-src', 'src'];
+			vue.transformAssetUrls.source = ['data-srcset', 'srcset'];
+		},
+	},
+
+	generate: {
+		fallback: '404.html',
+	},
 };
---
+++ pages/contact.vue
@@ -0,0 +1,75 @@
+<template>
+	<main>
+		<article>
+			<PageHeading :heading="teaName" summary="a collection of experiments I've built over the years" />
+
+			<section>
+				<form name="vue-tea" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+					<input type="hidden" name="form-name" value="vue-tea" />
+					<div class="tea">
+						<h2>Pick a Tea</h2>
+						<label
+							v-for="tea in teaNames"
+							:key="tea"
+							:class="{
+								'tea-label': true,
+								checked: tea === chosenTea,
+							}"
+						>
+							<input id="tea" v-model="chosenTea" name="tea" type="radio" :value="tea" />
+							<span>{{ tea }}</span>
+						</label>
+					</div>
+					<br />
+					<button @click="formSubmit" class="button">Submit</button>
+				</form>
+			</section>
+		</article>
+	</main>
+</template>
+
+<script>
+import PageHeading from '~/components/PageHeading.vue';
+
+export default {
+	name: 'Contact',
+	layout: 'default',
+	components: {
+		PageHeading,
+	},
+	data() {
+		return {
+			chosenTea: 'Earl Grey',
+			teaTypes: {
+				'Earl Grey': 'London',
+				'Irish Breakfast': 'Dublin',
+				Chai: 'Bombay',
+				Rose: 'Atlantic City',
+				Matcha: 'Tokyo',
+				Rooibos: 'Cape Town',
+				'Yerba Mate': 'Montreal',
+				Green: 'Oregon Mist',
+			},
+		};
+	},
+	computed: {
+		teaName() {
+			return `${this.teaTypes[this.chosenTea]} Fog`;
+		},
+		teaNames() {
+			return Object.keys(this.teaTypes);
+		},
+	},
+	methods: {
+		formSubmit() {
+			alert('SUBMITTED FORM');
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+form {
+	text-align: center;
+}
+</style>
